ddc411f2c374a5f3341596b04412817fdba6ec85
Badoino Matteo <48619446+BadoinoMatteo@users.noreply.github.com>
2020-05-15 15:44:51 +0200

    utilizzo dizionario per memorizzazione turtle

--- PHYTON/SOCKET/es4/server.py
+++ PHYTON/SOCKET/es4/server.py
@@ -5,33 +5,33 @@ import turtle
 localIP= "127.0.0.1"
 localPort= 80
 s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-lista=[]
+dict={}
 s.bind((localIP, localPort))
 listaComandi=['forward', 'backward', 'left', 'right']
 
 
 while True:
     ricevuto, addres = s.recvfrom(1024)
-    lista.append(addres)
-    lista.append(turtle.Turtle())
+    dict[addres]=(turtle.Turtle())
     stringa=ricevuto.decode().split('_')
-    if(addres in lista):
+    if(addres in dict):
         while True:
             variabile = (int)(stringa[1])
             if (listaComandi[0] == stringa[0]):
-                lista[lista.index(addres)+1].forward(variabile)
+                dict[addres].forward(variabile)
                 break
             if (listaComandi[1] == stringa[0]):
-                lista[lista.index(addres)+1].backward(variabile)
+                dict[addres].backward(variabile)
                 break
             if (listaComandi[2] == stringa[0]):
-                lista[lista.index(addres)+1].left(variabile)
+                dict[addres].left(variabile)
                 break
             if (listaComandi[3] == stringa[0]):
-                lista[lista.index(addres)+1].right(variabile)
+                dict[addres].right(variabile)
                 break
     else:
         print("non presente")
+    print("comando eseguito")
 
 
 s.close()
